cd9c856bee04a1863c1404fdc9e404c96e167cfc
SUSY <zx0596@naver.com>
2021-01-01 04:42:00 +0900

    del user

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import UsersViewSet
-app_name = "users"
-
-router = DefaultRouter()
-router.register("", UsersViewSet)
-urlpatterns = router.urls
