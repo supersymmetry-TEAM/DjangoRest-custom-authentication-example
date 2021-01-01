a61a7f67f47b7ec4e0bc851aab285b0b281e894c
supersymmetry-TEAM <62172128+supersymmetry-TEAM@users.noreply.github.com>
2021-01-01 10:37:08 +0900

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -2,18 +2,18 @@ import jwt
 from django.conf import settings
 from rest_framework import authentication
 from users.models import User
+from django.cotnrib.auth.models import User
 
-
-class JWTAuthentication(authentication.BaseAuthentication):
+class JWTBaseAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         try:
-            token = request.META.get("HTTP_AUTHORIZATION")
+            token = request.META.get("HTTP_AUTHORIZATION") # check token with in header
             if token is None:
                 return None
-            _, jwt_token = token.split(" ")
-            decoded = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=["HS256"])
-            pk = decoded.get("pk")
-            user = User.objects.get(pk=pk)
-            return (user, None)
+            _, jwt_token = token.split(" ") # the token generally is sended Basic + token or Bear + token so we need to parse
+            decoded = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=["HS256"]) #decode the token
+            pk = decoded.get("pk") 
+            user = User.objects.get(pk=pk) 
+            return (user, None) 
         except (ValueError, jwt.exceptions.DecodeError, User.DoesNotExist):
             return None
